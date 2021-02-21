a317dbe168195f66b3cc90946da3270bac7e3359
Abirk2day <abirk2day@gmail.com>
2021-02-21 21:36:37 +0600

    advench

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,11 +1,29 @@
-import React from 'react';
+// variable: let , const;
+let money = 20;
+money = 12;
+const name = 'abdul kudu'
 
-const Fundamentals = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+let isPlaying = false;
 
-export default Fundamentals;
+const person = {
+    name:'kudes',
+    age:55,
+    city: 'dhaka',
+    isPlaying: true
+}
+
+
+const statemnet = ` this person is ${name}  her age is most import ${person.age}`;
+// condition
+
+if(money >= 20 && age>45){
+
+}
+else{
+
+}
+
+
+
+
+const array = [45, 655, 645, 44, ]
