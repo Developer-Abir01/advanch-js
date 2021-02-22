7e3ad779efed2ebc6c8c198490f7e42c30d56a13
Abirk2day <abirk2day@gmail.com>
2021-02-22 15:29:19 +0600

    map

---
+++ map-filter-find-forEach.js
@@ -0,0 +1,9 @@
+const number = [2, 4 ,5, 78, 4];
+const friend = ['rafiq', 'jabarr', 'abir'];
+const product = [
+    {id:1, name:'laptop', price:564},
+    {id:2, name:'mobile', price:100},
+    {id:3, name:'TV', price:44}
+];
+const names = product.map(pd => pd.name);
+console.log(names)
